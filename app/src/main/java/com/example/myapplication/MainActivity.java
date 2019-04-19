d4a29df45b36fad1b7d5bd1d649c11de870fc6e0
sharmanirudh <sharmanirudh27@gmail.com>
2019-04-19 21:50:49 +0530

    measure WiFi dBm value

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,17 +26,14 @@ public class MainActivity extends AppCompatActivity {
     private int y = 1;
     private TextView textViewXValue;
     private TextView textViewYValue;
-    private Button buttonIncreaseX;
-    private Button buttonDecreaseX;
-    private Button buttonIncreaseY;
-    private Button buttonDecreaseY;
     private RelativeLayout relativeLayoutMap;
     private RelativeLayout relativeLayoutPointer;
     private View viewPointer;
 
-    private EditText editTextBSSID;
-    private EditText editTextDBm;
-    private Button buttonLocate;
+    private Button buttonStart;
+    private TextView textViewSSID;
+    private TextView textViewBSSID;
+    private TextView textViewDBm;
     private Router [] router;
     private Coordinate current_coordinate;
     private int old_dBm;
@@ -42,57 +43,27 @@ public class MainActivity extends AppCompatActivity {
     private String BSSID;
     private String SSID;
     int dBm;
+    boolean start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    0);
+        }
 
         relativeLayoutMap = findViewById(R.id.relativeLayoutMap);
         relativeLayoutPointer = findViewById(R.id.relativeLayoutPointer);
         viewPointer = findViewById(R.id.viewPointer);
+        buttonStart = findViewById(R.id.buttonStart);
+        textViewSSID = findViewById(R.id.textViewSSID);
+        textViewBSSID = findViewById(R.id.textViewBSSID);
+        textViewDBm = findViewById(R.id.textViewDBm);
         textViewXValue = findViewById(R.id.textViewXValue);
         textViewYValue = findViewById(R.id.textViewYValue);
-        buttonIncreaseX = findViewById(R.id.buttonIncreaseX);
-        buttonIncreaseX.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                if (x < 40) {
-                    x++;
-                    textViewXValue.setText("" + x);
-                    movePointer(x, y, 1);
-                }
-            }
-        });
-        buttonDecreaseX = findViewById(R.id.buttonDecreaseX);
-        buttonDecreaseX.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                if (x > 1) {
-                    x--;
-                    textViewXValue.setText("" + x);
-                    movePointer(x, y, 1);
-                }
-            }
-        });
-        buttonDecreaseY = findViewById(R.id.buttonDecreaseY);
-        buttonDecreaseY.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                if (y > 1) {
-                    y--;
-                    textViewYValue.setText("" + y);
-                    movePointer(x, y, 1);
-                }
-            }
-        });
-        buttonIncreaseY = findViewById(R.id.buttonIncreaseY);
-        buttonIncreaseY.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                if (y < 40) {
-                    y++;
-                    textViewYValue.setText("" + y);
-                    movePointer(x, y, 1);
-                }
-            }
-        });
 
         router = new Router[7];
         router[0] = new Router();
@@ -131,8 +102,59 @@ public class MainActivity extends AppCompatActivity {
         router[1].coordinate[11] = new Coordinate(-38, 24, 49);
         router[1].coordinate[12] = new Coordinate(-44, 27, 49);
 
+        router[2] = new Router();
+        router[2].bssid = "54:3d:37:d4:68:f8";
+        router[2].length = 5;
+        router[2].coordinate = new Coordinate[5];
+        router[2].coordinate[0] = new Coordinate(-35, 30, 49);
+        router[2].coordinate[1] = new Coordinate(-30, 33, 49);
+        router[2].coordinate[2] = new Coordinate(-33, 36, 49);
+        router[2].coordinate[3] = new Coordinate(-35, 39, 49);
+        router[2].coordinate[4] = new Coordinate(-41, 42, 49);
+
+        router[3] = new Router();
+        router[3].bssid = "d4:68:4d:ff:0f:a8";
+        router[3].length = 6;
+        router[3].coordinate = new Coordinate[6];
+        router[3].coordinate[0] = new Coordinate(-36, 48, 49);
+        router[3].coordinate[1] = new Coordinate(-49, 48, 46);
+        router[3].coordinate[2] = new Coordinate(-47, 48, 43);
+        router[3].coordinate[3] = new Coordinate(-46, 48, 40);
+        router[3].coordinate[4] = new Coordinate(-47, 48, 37);
+        router[3].coordinate[5] = new Coordinate(-49, 48, 34);
+
+        router[4] = new Router();
+        router[4].bssid = "54:3d:37:d5:07:78";
+        router[4].length = 11;
+        router[4].coordinate = new Coordinate[11];
+        router[4].coordinate[0] = new Coordinate(-41, 48, 31);
+        router[4].coordinate[1] = new Coordinate(-41, 48, 28);
+        router[4].coordinate[2] = new Coordinate(-33, 48, 25);
+        router[4].coordinate[3] = new Coordinate(-28, 48, 22);
+        router[4].coordinate[4] = new Coordinate(-33, 48, 19);
+        router[4].coordinate[5] = new Coordinate(-41, 48, 16);
+        router[4].coordinate[6] = new Coordinate(-43, 48, 13);
+        router[4].coordinate[7] = new Coordinate(-49, 48, 10);
+        router[4].coordinate[8] = new Coordinate(-48, 48, 7);
+        router[4].coordinate[9] = new Coordinate(-52, 48, 4);
+        router[4].coordinate[10] = new Coordinate(-54, 48, 0);
+
+        router[5] = new Router();
+        router[5].bssid = "54:3d:37:d5:07:a8";
+        router[5].length = 9;
+        router[5].coordinate = new Coordinate[9];
+        router[5].coordinate[0] = new Coordinate(-35, 48, 0);
+        router[5].coordinate[1] = new Coordinate(-37, 45, 0);
+        router[5].coordinate[2] = new Coordinate(-45, 42, 0);
+        router[5].coordinate[3] = new Coordinate(-45, 39, 0);
+        router[5].coordinate[4] = new Coordinate(-51, 36, 0);
+        router[5].coordinate[5] = new Coordinate(-55, 33, 0);
+        router[5].coordinate[6] = new Coordinate(-49, 30, 0);
+        router[5].coordinate[7] = new Coordinate(-55, 27, 0);
+        router[5].coordinate[8] = new Coordinate(-55, 24, 0);
+
         router[6] = new Router();
-        router[6].bssid = "84:18:3a:dc:68:88";
+        router[6].bssid = "54:3d:37:d9:f6:88";
         router[6].length = 8;
         router[6].coordinate = new Coordinate[8];
         router[6].coordinate[0] = new Coordinate(-28, 21, 0);
@@ -144,84 +166,173 @@ public class MainActivity extends AppCompatActivity {
         router[6].coordinate[6] = new Coordinate(-52, 3, 0);
         router[6].coordinate[7] = new Coordinate(-53, 0, 0);
 
+//        buttonLocate = findViewById(R.id.buttonLocate);
+//        buttonLocate.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                String BSSID = editTextBSSID.getText().toString();
+//                int dBm = - Math.abs(Integer.parseInt(editTextDBm.getText().toString()));
+//                int i;
+//                for (i = 0; i < 3; i++) {
+//                    if (BSSID.equals(router[i].bssid))
+//                        break;
+//                }
+//                if ((dBm == old_dBm && current_BSSID.equals(router[i].bssid)) || (dBm == current_coordinate.dbm && current_BSSID.equals(router[i].bssid)) || i == 4)
+//                    return;
+//                Coordinate [] neighbours = new Coordinate[8];
+//                int j, k, l;
+//                for (j = iInRouter + 1, l = iInRouter - 1, k = 0; j < Math.min(router[i].length, iInRouter + 5) && l >= Math.max(0, iInRouter - 4); l--, j++) {
+//                    neighbours[k++] = router[i].coordinate[j];
+//                    neighbours[k++] = router[i].coordinate[l];
+//                }
+//                if (j == Math.min(router[i].length, iInRouter + 5)) {
+//                    while (l >= Math.max(0, iInRouter - 4)) {
+//                        neighbours[k++] = new Coordinate(+1, -1, -1);
+//                        neighbours[k++] = router[i].coordinate[l--];
+//                    }
+//                }
+//                if (l == Math.max(0, iInRouter - 4 - 1)) {
+//                    while (j < Math.min(router[i].length, iInRouter + 5)) {
+//                        neighbours[k++] = router[i].coordinate[j++];
+//                        neighbours[k++] = new Coordinate(+1, -1, -1);
+//                    }
+//                }
+//                String str = "[";
+//                for (j = 0; j < k; j++) {
+//                    if (j % 2 == 0) {
+//                        str = str + "(" + neighbours[j].dbm;
+//                    }
+//                    else {
+//                        str = str + ", " + neighbours[j].dbm + "), ";
+//                    }
+//                }
+//                str = str + "]";
+//                Log.d(TAG, "onClick: neighbours[] : " + str);
+//                Coordinate closestCoordinate = current_coordinate;
+//                int minDifference = 99999;
+//                int pos = iInRouter;
+//                for (j = 0; j < Math.min(2 * 3, k) ; j++) {//considering only 3 steps
+//                    if (neighbours[j].dbm == 1)
+//                        continue;
+//                    if (minDifference > Math.abs(neighbours[j].dbm - dBm)) {
+//                        minDifference = Math.abs(neighbours[j].dbm - dBm);
+//                        closestCoordinate = neighbours[j];
+//                        pos = j;
+//                    }
+//                }
+//                old_dBm = dBm;
+//                current_coordinate = closestCoordinate;
+//                iInRouter = (pos % 2 == 0) ? iInRouter + pos + 1 : iInRouter - pos;
+//                current_BSSID = router[i].bssid;
+//                x = current_coordinate.x;
+//                y = current_coordinate.y;
+//                textViewXValue.setText("" + x);
+//                textViewYValue.setText("" + y);
+//                movePointer(x, y, 1);
+//            }
+//        });
 
-        current_coordinate = new Coordinate(-55, 0, 21);
-        old_dBm = current_coordinate.dbm;
-        current_BSSID = router[0].bssid;
-        x = current_coordinate.x;
-        y = current_coordinate.y;
-        textViewXValue.setText("" + x);
-        textViewYValue.setText("" + y);
-        movePointer(x, y, 1);
-        iInRouter = 7;
-
-        editTextBSSID = findViewById(R.id.editTextBSSID);
-        editTextDBm = findViewById(R.id.editTextDBm);
-        buttonLocate = findViewById(R.id.buttonLocate);
-        buttonLocate.setOnClickListener(new Button.OnClickListener() {
+        start = false;
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                String BSSID = editTextBSSID.getText().toString();
-                int dBm = - Math.abs(Integer.parseInt(editTextDBm.getText().toString()));
-                int i;
-                for (i = 0; i < 3; i++) {
-                    if (BSSID.equals(router[i].bssid))
-                        break;
+                start = !start;
+                if (start) { // 0, 0 location
+                    buttonStart.setText("stop");
+                    iInRouter = 7;
+                    current_coordinate = router[6].coordinate[iInRouter];
+                    current_BSSID = router[6].bssid;
+                    old_dBm = current_coordinate.dbm;
+                    x = current_coordinate.x;
+                    y = current_coordinate.y;
+                    textViewXValue.setText("" + x);
+                    textViewYValue.setText("" + y);
+                    movePointer(x, y, 1);
+                } else {
+                    buttonStart.setText("start");
+                    textViewXValue.setText("");
+                    textViewYValue.setText("");
                 }
-                if ((dBm == old_dBm && current_BSSID.equals(router[i].bssid)) || (dBm == current_coordinate.dbm && current_BSSID.equals(router[i].bssid)) || i == 4)
-                    return;
-                Coordinate [] neighbours = new Coordinate[8];
-                int j, k, l;
-                for (j = iInRouter + 1, l = iInRouter - 1, k = 0; j < Math.min(router[i].length, iInRouter + 5) && l >= Math.max(0, iInRouter - 4); l--, j++) {
-                    neighbours[k++] = router[i].coordinate[j];
-                    neighbours[k++] = router[i].coordinate[l];
-                }
-                if (j == Math.min(router[i].length, iInRouter + 5)) {
-                    while (l >= Math.max(0, iInRouter - 4)) {
-                        neighbours[k++] = new Coordinate(+1, -1, -1);
-                        neighbours[k++] = router[i].coordinate[l--];
-                    }
-                }
-                if (l == Math.max(0, iInRouter - 4 - 1)) {
-                    while (j < Math.min(router[i].length, iInRouter + 5)) {
-                        neighbours[k++] = router[i].coordinate[j++];
-                        neighbours[k++] = new Coordinate(+1, -1, -1);
-                    }
-                }
-                String str = "[";
-                for (j = 0; j < k; j++) {
-                    if (j % 2 == 0) {
-                        str = str + "(" + neighbours[j].dbm;
-                    }
-                    else {
-                        str = str + ", " + neighbours[j].dbm + "), ";
-                    }
-                }
-                str = str + "]";
-                Log.d(TAG, "onClick: neighbours[] : " + str);
-                Coordinate closestCoordinate = current_coordinate;
-                int minDifference = 99999;
-                int pos = iInRouter;
-                for (j = 0; j < Math.min(2 * 3, k) ; j++) {//considering only 3 steps
-                    if (neighbours[j].dbm == 1)
-                        continue;
-                    if (minDifference > Math.abs(neighbours[j].dbm - dBm)) {
-                        minDifference = Math.abs(neighbours[j].dbm - dBm);
-                        closestCoordinate = neighbours[j];
-                        pos = j;
-                    }
-                }
-                old_dBm = dBm;
-                current_coordinate = closestCoordinate;
-                iInRouter = (pos % 2 == 0) ? iInRouter + pos + 1 : iInRouter - pos;
-                current_BSSID = router[i].bssid;
-                x = current_coordinate.x;
-                y = current_coordinate.y;
-                textViewXValue.setText("" + x);
-                textViewYValue.setText("" + y);
-                movePointer(x, y, 1);
             }
         });
 
+        final WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        final Handler handler = new Handler();
+        int delay = 100; //milliseconds
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                if(wifiInfo.getRssi() == -127) {
+                    SSID = "Not connected to WiFi";
+                    BSSID = "Connection required!";
+                } else {
+                    SSID = wifiInfo.getSSID();
+                    BSSID = wifiInfo.getBSSID();
+                    dBm = wifiInfo.getRssi();
+                    textViewSSID.setText(SSID);
+                    textViewBSSID.setText(BSSID);
+                    textViewDBm.setText(String.format("%s", dBm));
+                    if (start) {
+                        int i;
+                        for (i = 0; i < 7; i++) {
+                            if (BSSID.equals(router[i].bssid))
+                                break;
+                        }
+                        if ((dBm == old_dBm && current_BSSID.equals(router[i].bssid)) || (dBm == current_coordinate.dbm && current_BSSID.equals(router[i].bssid)) || i == 7)
+                            return;
+                        Coordinate[] neighbours = new Coordinate[8];
+                        int j, k, l;
+                        for (j = iInRouter + 1, l = iInRouter - 1, k = 0; j < Math.min(router[i].length, iInRouter + 5) && l >= Math.max(0, iInRouter - 4); l--, j++) {
+                            neighbours[k++] = router[i].coordinate[j];
+                            neighbours[k++] = router[i].coordinate[l];
+                        }
+                        if (j == Math.min(router[i].length, iInRouter + 5)) {
+                            while (l >= Math.max(0, iInRouter - 4)) {
+                                neighbours[k++] = new Coordinate(+1, -1, -1);
+                                neighbours[k++] = router[i].coordinate[l--];
+                            }
+                        }
+                        if (l == Math.max(0, iInRouter - 4 - 1)) {
+                            while (j < Math.min(router[i].length, iInRouter + 5)) {
+                                neighbours[k++] = router[i].coordinate[j++];
+                                neighbours[k++] = new Coordinate(+1, -1, -1);
+                            }
+                        }
+                        String str = "[";
+                        for (j = 0; j < k; j++) {
+                            if (j % 2 == 0) {
+                                str = str + "(" + neighbours[j].dbm;
+                            } else {
+                                str = str + ", " + neighbours[j].dbm + "), ";
+                            }
+                        }
+                        str = str + "]";
+                        Log.d(TAG, "onClick: neighbours[] : " + str);
+                        Coordinate closestCoordinate = current_coordinate;
+                        int minDifference = 99999;
+                        int pos = iInRouter;
+                        for (j = 0; j < Math.min(2 * 3, k); j++) {//considering only 3 steps
+                            if (neighbours[j].dbm == 1)
+                                continue;
+                            if (minDifference > Math.abs(neighbours[j].dbm - dBm)) {
+                                minDifference = Math.abs(neighbours[j].dbm - dBm);
+                                closestCoordinate = neighbours[j];
+                                pos = j;
+                            }
+                        }
+                        old_dBm = dBm;
+                        current_coordinate = closestCoordinate;
+                        iInRouter = (pos % 2 == 0) ? iInRouter + pos + 1 : iInRouter - pos;
+                        current_BSSID = router[i].bssid;
+                        x = current_coordinate.x;
+                        y = current_coordinate.y;
+                        textViewXValue.setText("" + x);
+                        textViewYValue.setText("" + y);
+                        movePointer(x, y, 1);
+                    }
+                }
+                handler.postDelayed(this, 1000);
+            }
+        }, delay);
 
     }
 
